b06425faca44481d00d09ebececec4ce7ed97d22
Victor Santos <victorsa.mg@gmail.com>
2019-08-24 17:23:25 -0300

    Migração de criação da tabela de usuários

---
+++ src/database/migrations/20190824195354-create-users.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Criação da tabela de usuários.
+    */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    /*
+      Remoção da tabela de usuários.
+     */
+    return queryInterface.dropTable('users');
+  },
+};

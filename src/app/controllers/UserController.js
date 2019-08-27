ff69baea2a5ce8a483263202e68cbd3710f59936
Victor Santos <victorsa.mg@gmail.com>
2019-08-26 23:50:52 -0300

    Implementado o cadastro de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,16 +2,20 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    // checagem se existe usuário com o e-mail enviado
+    // checagem se existe um usuário cadastrado com o e-mail fornecido
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res
-        .status(400)
-        .json({ error: 'Já existe um usuário com o e-mail informado.' });
+      return res.status(400).json({
+        error: 'Já existe um usuário cadastrado com o e-mail informado.',
+      });
     }
 
-    return res.json({ message: 'Usuário pode ser cadastrado.' });
+    // cadastra o usuário no banco de dados
+    const { id, name, email } = await User.create(req.body);
+
+    // informa que o usuário foi cadastrado
+    return res.json({ id, name, email, message: 'Usuário cadastrado' });
   }
 }
 

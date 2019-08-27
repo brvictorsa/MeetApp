88e894dbe9f318fe54d1488efdf3dc768af756a0
Victor Santos <victorsa.mg@gmail.com>
2019-08-27 00:11:33 -0300

    Geração do hash do password do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,12 +7,22 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    // adiciona um hook antes de salvar o usuário
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        // gera o hash de senha
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
   }
 }

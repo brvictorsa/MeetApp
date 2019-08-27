211b41ebe059d70cbc4c5ca8e0394452d86cc82b
Victor Santos <victorsa.mg@gmail.com>
2019-08-26 22:59:52 -0300

    Criado o model de usuários

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,14 @@ import { Router } from 'express';
 const routes = new Router();
 
 // welcome route
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Meetapp application says hello' });
+// routes.get('/', (req, res) => {
+//   return res.json({ message: 'Meetapp application says hello' });
+// });
+
+routes.post('/users', (req, res) => {
+  const { name, email, password } = req.body;
+
+  return res.json({ name, email, password });
 });
 
 export default routes;
